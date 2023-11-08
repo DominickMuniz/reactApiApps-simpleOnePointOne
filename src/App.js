ee6e86990e19a30ce1d1c49cf84ced244a771552
Dominick Muniz <dominick.muniz7@gmail.com>
2023-11-08 01:44:52 -0700

    middle of figuring it out.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [excuse, setExcuse] = useState("");
+  const [reason, setReason] = useState(null);
+
+  const fetchData = (event) => {
+    const excuseValue = event.target.textContent.trim();
+    setExcuse(excuseValue);
+
+    axios
+      .get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`)
+      .then((res) => {
+        setReason(res.data.excuse);
+        console.log(res.data);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h2> Generate an excuse </h2>
+        <button className="inputNameButton" onClick={fetchData}>
+          Family
+        </button>
+        <h2> Here is an excuse to get out of your {excuse} event:</h2>
+        <p>{reason} </p>
+      </div>
     </div>
   );
 }
