69a78a71acccac546ab1f204f6dba25f9f8f1135
Dominick Muniz <dominick.muniz7@gmail.com>
2023-11-08 02:18:14 -0700

    works great!!!!

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 /** @format */
 
-import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [excuse, setExcuse] = useState("");
+  const [situation, setSituation] = useState("");
   const [reason, setReason] = useState(null);
 
   const fetchData = (event) => {
-    const excuseValue = event.target.textContent.trim();
-    setExcuse(excuseValue);
+    var situationValue = event.target.textContent.trim();
+    situationValue = situationValue.toLowerCase();
+    console.log(situationValue);
+    setSituation(situationValue);
 
     axios
-      .get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`)
+      .get(`https://excuser-three.vercel.app/v1/excuse/${situationValue}/`)
       .then((res) => {
-        setReason(res.data.excuse);
+        setReason(res.data[0]);
         console.log(res.data);
       });
   };
@@ -28,8 +29,10 @@ function App() {
         <button className="inputNameButton" onClick={fetchData}>
           Family
         </button>
-        <h2> Here is an excuse to get out of your {excuse} event:</h2>
-        <p>{reason} </p>
+        <h2> Here is an excuse to get out of your {reason?.category} event:</h2>
+        <p>{reason?.excuse} </p>
+        <h3>All fetched data:</h3>
+        <pre>{JSON.stringify(reason, null, 2)}</pre>
       </div>
     </div>
   );
